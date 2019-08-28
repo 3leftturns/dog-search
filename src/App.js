41c3a97f9a35126214eea8c059a57fc80f823b61
andyjohnson <andy.johnson@domo.com>
2019-08-28 11:38:52 -0600

    first com-mutt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      labradors : []
+    }
+  }
+
+  dogs = require("./dogs.json");
+
+  fetchLabradors = (dogs) => {
+    let labradors = [];
+
+    //your code here
+    if (labradors.length > 0) {
+    this.setState({labradors : []})
+  }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>List of Labradors!</p>
+        <p>{this.state.labradors && JSON.stringify(this.state.labradors)}</p>
+        <button onClick={this.fetchLabradors(this.dogs)}>Fetch!</button>
+      </div>
+    );
+  }
 }
-
 export default App;
